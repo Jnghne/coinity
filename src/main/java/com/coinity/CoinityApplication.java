9ea2ddfe1858e0e847c5c3c723e6bfb14174f680
Kyle <jh13012@gmail.com>
2025-04-08 23:14:50 +0900

    First Commit

---
+++ src/main/java/com/coinity/CoinityApplication.java
@@ -0,0 +1,13 @@
+package com.coinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinityApplication.class, args);
+    }
+
+}
